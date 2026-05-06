98e306eac5d4209e8d43e80e5f6277b213730040
vvelt <pavlovvasa860@gmail.com>
2026-05-06 21:44:06 +1000

    fix ava + name + bio

--- src/Body Components/AvaNameBio.tsx
+++ src/Body Components/AvaNameBio.tsx
@@ -7,38 +7,43 @@ interface Props {
 export default function AvaNameBio({results}: Props) {
     const [imgError, setImgError] = useState<boolean>(false)
     return (
-        <div className='flex items-center relative'>
-            {imgError ? (
-                <div className='rounded-full h-72 w-72 bg-gray-300 dark:bg-gray-700  flex items-center justify-center text-6xl mt-9 mx-9 object-cover object-center'>
-                    {results.login[0].toUpperCase()}
-                </div>
+        <div className='grid grid-cols-[auto_200px_1fr] items-center gap-6 p-6'>
+
+            <div className='flex-shrink-0'>
+                {imgError ? (
+                    <div className='rounded-full h-72 w-72 bg-gray-700 flex items-center justify-center text-6xl'>
+                        {results.login[0].toUpperCase()}
+                    </div>
                 ) : (
-                <img src={results.avatar_url}
-                     onError={() => setImgError(true)}
-                     className='rounded-full object-cover object-center self-start h-72 w-72 mt-9 mx-9 '
-                />
-            )}
-            <div className='flex flex-col ml-3'>
-                <div className='font-bold text-3xl text-gray-500 dark:text-white/50 italic absolute -mt-8'>
-                    {results.name}
-                </div>
-                <div className='font-bold text-5xl '>
-                    <a href={results.html_url} target='_blank' rel='noreferrer' className='font-bold text-5xl  text-gray-700 dark:text-white no-underline'>
-                        {results.login}
-                    </a>
-                </div>
+                    <img
+                        src={results.avatar_url}
+                        onError={() => setImgError(true)}
+                        className='rounded-full object-cover object-center h-72 w-72'
+                    />
+                )}
             </div>
 
-
-            {results.bio !== null ?
-                <div className='font-bold mr-9 line-clamp-7 mx-auto'>{results.bio}</div>
-                :
-                <div className='font-bold mx-auto line-clamp-7 gap-2 flex flex-col text-2xl'>
-                    <span>👥 {results.followers} followers</span>
-                    <span>➕ {results.following} following</span>
-                    <span>📁 {results.public_repos} repos</span>
+            <div className='flex flex-col relative ml-5'>
+                <div className='font-bold text-2xl text-gray-500 dark:text-white/50 italic truncate'>
+                    {results.name}
                 </div>
-            }
+                <a href={results.html_url} target='_blank' rel='noreferrer' className='font-bold text-4xl dark:text-white truncate'>
+                    {results.login}
+                </a>
+            </div>
+
+            <div className='min-w-0'>
+                {results.bio !== null ?
+                    <div className='font-bold line-clamp-4'>{results.bio}</div>
+                    :
+                    <div className='flex flex-col gap-2 text-2xl font-bold'>
+                        <span>👥 {results.followers} followers</span>
+                        <span>➕ {results.following} following</span>
+                        <span>📁 {results.public_repos} repos</span>
+                    </div>
+                }
+            </div>
+
         </div>
     )
 }
